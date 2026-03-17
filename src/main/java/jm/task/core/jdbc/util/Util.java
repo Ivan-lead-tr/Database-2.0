1f7b35b521726270eeed8f0e9fc41ae120e19435
Ivan-lead-tr <bitiv@mail.ru>
2026-03-17 19:44:21 +0300

    Add update

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -8,29 +8,11 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 import org.hibernate.service.ServiceRegistry;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.Properties;
 
 public class Util {
-    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://localhost:3306/mybdtest";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "root";
 
     private static volatile SessionFactory sessionFactory;
-
-    public static Connection getConnection() {
-        try {
-            Class.forName(DRIVER);
-            Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            System.out.println("Соединение установлено с БД");
-            return connection;
-        } catch (SQLException | ClassNotFoundException e) {
-            throw new RuntimeException("Ошибка соединения с БД", e);
-        }
-    }
 
     public static SessionFactory getSessionFactory() {
         if (sessionFactory == null){
